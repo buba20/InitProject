7e6fcc73632778029c80e2bece5dfb810a79e55c
buba20 <buba20@gmail.com>
2016-04-01 16:45:09 +0200

    typing

---
+++ typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser/ambient/angular-material/index.d.ts" />
+/// <reference path="browser/ambient/angular-ui-router/index.d.ts" />
+/// <reference path="browser/ambient/angular/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
